cbb5c926c1886a0e04381cc52002873e5678a29e
Kemish <llo14007@byui.edu>
2021-06-29 19:17:47 -0600

    Added Cooper and Lloyd

--- src/lloyd/Main.java
+++ src/lloyd/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello, my name is Kemish");
+        System.out.println("Hello, my name is Cooper");
+        System.out.println("Hello, my name is Lloyd");
     }
 }
